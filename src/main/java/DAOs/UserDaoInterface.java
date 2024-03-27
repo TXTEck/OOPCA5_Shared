69df86d389348924d6133d278e775e0ed4d9d562
Bianca Valicec <biavalicec@gmail.com>
2024-03-27 14:23:43 +0000

    fixed up the interface file by removing the unnecessary public in the code as well as adding the new method. Added feature 8 to the menu and the method for calling it to the app class. Added another catch in the methods containing scanners to catch InputMismatchException

--- src/main/java/DAOs/UserDaoInterface.java
+++ src/main/java/DAOs/UserDaoInterface.java
@@ -5,16 +5,19 @@ import Exceptions.DaoException;
 
 import java.util.Comparator;
 import java.util.List;
-public interface UserDaoInterface {
-    public List<User> findAllUsers() throws DaoException;
 
-    public User findUserByStudentId(int studentId) throws DaoException;
+public interface UserDaoInterface {
+    List<User> findAllUsers() throws DaoException;
+
+    User findUserByStudentId(int studentId) throws DaoException;
 
     boolean deleteUserByStudentId(int studentId) throws DaoException;
 
-    public User insertUser(User user) throws DaoException;
+    User insertUser(User user) throws DaoException;
 
     void updateUserByStudentId(int studentId, User user) throws DaoException;
 
-    public List<User> findUsersUsingFilter(Comparator<User> comparator) throws DaoException;
+    List<User> findUsersUsingFilter(Comparator<User> comparator) throws DaoException;
+
+    String findUserJsonByStudentId(int studentId) throws DaoException;
 }
--- src/main/java/Objects/App.java
+++ src/main/java/Objects/App.java
@@ -1,6 +1,6 @@
 package Objects;
 
-
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import Comparators.UserGradeComparator;
 import DAOs.MySqlUserDao;
@@ -9,10 +9,10 @@ import DTOs.User;
 import Exceptions.DaoException;
 import java.util.List;
 
-public class App
-{
+public class App {
     private static final Scanner scanner = new Scanner(System.in);
     private static final UserDaoInterface IUserDao = new MySqlUserDao();
+
     public static void main(String[] args) {
         displayMenu();
     }
@@ -20,55 +20,64 @@ public class App
     private static void displayMenu() {
         int choice;
         do {
-            System.out.println("\n---- Menu ----");
-            System.out.println("1. Find All Users");
-            System.out.println("2. Find User By Student ID");
-            System.out.println("3. Delete User By Student ID");
-            System.out.println("4. Insert New User");
-            System.out.println("5. Update User By Student ID");
-            System.out.println("6. Find Users Using Filter");
-            System.out.println("7. Exit");
-            System.out.print("Enter your choice: ");
+            try {
+                System.out.println("\n---- Menu ----");
+                System.out.println("1. Find All Users");
+                System.out.println("2. Find User By Student ID");
+                System.out.println("3. Delete User By Student ID");
+                System.out.println("4. Insert New User");
+                System.out.println("5. Update User By Student ID");
+                System.out.println("6. Find Users Using Filter");
+                System.out.println("8. Convert a single user to JSON");
+                System.out.println("0. Exit");
+                System.out.print("Enter your choice: ");
 
-            // Read user choice
-            choice = scanner.nextInt();
-            scanner.nextLine(); // Consume newline
+                // Read user choice
+                choice = scanner.nextInt();
+                scanner.nextLine(); // Consume newline
 
-            // Perform action based on choice
-            switch (choice) {
-                case 1:
-                    findAllUsers();
-                    break;
-                case 2:
-                    findUserByStudentId();
-                    break;
-                case 3:
-                    deleteUserByStudentId();
-                    break;
-                case 4:
-                    insertNewUser();
-                    break;
-                case 5:
-                    updateUserByStudentId();
-                    break;
-                case 6:
-                    findUsersUsingFilter();
-                    break;
-                case 7:
-                    System.out.println("Exiting...");
-                    break;
-                default:
-                    System.out.println("Invalid choice. Please try again.");
+                // Perform action based on choice
+                switch (choice) {
+                    case 1:
+                        findAllUsers();
+                        break;
+                    case 2:
+                        findUserByStudentId();
+                        break;
+                    case 3:
+                        deleteUserByStudentId();
+                        break;
+                    case 4:
+                        insertNewUser();
+                        break;
+                    case 5:
+                        updateUserByStudentId();
+                        break;
+                    case 6:
+                        findUsersUsingFilter();
+                        break;
+                    case 8:
+                        convertUserToJson();
+                        break;
+                    case 0:
+                        System.out.println("Exiting...");
+                        break;
+                    default:
+                        System.out.println("Invalid choice. Please try again.");
+                }
+            } catch (InputMismatchException e) {
+                // Handle incorrect input
+                System.out.println("Invalid input. Please enter a valid choice.");
+                scanner.nextLine(); // Clear the invalid input from the scanner
+                choice = -1; // Reset the choice to trigger the loop to continue
             }
-        } while (choice != 7);
+        } while (choice != 0);
     }
 
     private static void findAllUsers() {
         try {
             System.out.println("\n--- Finding all users ---");
-            // Call DAO method to find all users
             List<User> users = IUserDao.findAllUsers();
-            // Display users
             displayUsers(users);
         } catch (DaoException e) {
             System.out.println("Error: " + e.getMessage());
@@ -81,13 +90,13 @@ public class App
             System.out.print("Enter student ID: ");
             int studentId = scanner.nextInt();
             scanner.nextLine(); // Consume newline
-
-            // Call DAO method to find user by student ID
             User user = IUserDao.findUserByStudentId(studentId);
-            // Display user
             displayUser(user);
         } catch (DaoException e) {
             System.out.println("Error: " + e.getMessage());
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input. Please enter a valid student ID.");
+            scanner.nextLine(); // Clear the invalid input from the scanner
         }
     }
 
@@ -97,8 +106,6 @@ public class App
             System.out.print("Enter student ID to delete: ");
             int studentId = scanner.nextInt();
             scanner.nextLine(); // Consume newline
-
-            // Call DAO method to delete user by student ID
             boolean deletionResult = IUserDao.deleteUserByStudentId(studentId);
             if (deletionResult) {
                 System.out.println("User with student ID " + studentId + " deleted successfully.");
@@ -107,13 +114,15 @@ public class App
             }
         } catch (DaoException e) {
             System.out.println("Error: " + e.getMessage());
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input. Please enter a valid student ID.");
+            scanner.nextLine(); // Clear the invalid input from the scanner
         }
     }
 
     private static void insertNewUser() {
         try {
             System.out.println("\n--- Inserting new user ---");
-            // Read user details
             System.out.print("Enter student ID: ");
             int studentId = scanner.nextInt();
             scanner.nextLine(); // Consume newline
@@ -132,13 +141,14 @@ public class App
             System.out.print("Enter semester: ");
             String semester = scanner.nextLine();
 
-            // Create User object
             User newUser = new User(studentId, firstName, lastName, courseId, courseName, grade, semester);
-            // Call DAO method to insert user
             User insertedUser = IUserDao.insertUser(newUser);
             System.out.println("User inserted successfully. ID: " + insertedUser.getId());
         } catch (DaoException e) {
             System.out.println("Error: " + e.getMessage());
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input. Please enter valid data.");
+            scanner.nextLine(); // Clear the invalid input from the scanner
         }
     }
 
@@ -149,7 +159,6 @@ public class App
             int studentIdToUpdate = scanner.nextInt();
             scanner.nextLine(); // Consume newline
 
-            // Read updated user details
             System.out.print("Enter new first name: ");
             String newFirstName = scanner.nextLine();
             System.out.print("Enter new last name: ");
@@ -165,15 +174,17 @@ public class App
             System.out.print("Enter new semester: ");
             String newSemester = scanner.nextLine();
 
-            // Create updated User object
             User updatedUser = new User(newFirstName, newLastName, newCourseId, newCourseName, newGrade, newSemester);
-            // Call DAO method to update user
             IUserDao.updateUserByStudentId(studentIdToUpdate, updatedUser);
             System.out.println("User updated successfully.");
         } catch (DaoException e) {
             System.out.println("Error: " + e.getMessage());
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input. Please enter valid data.");
+            scanner.nextLine(); // Clear the invalid input from the scanner
         }
     }
+
 
     private static void findUsersUsingFilter() {
         try {
@@ -203,6 +214,22 @@ public class App
                     user.getCourseId(), user.getCourseName(), user.getGrade(), user.getSemester());
         } else {
             System.out.println("User not found.");
+        }
+    }
+
+    private static void convertUserToJson() {
+        try {
+            System.out.println("\n--- Converting user to JSON ---");
+            System.out.print("Enter student ID: ");
+            int studentId = scanner.nextInt();
+            scanner.nextLine(); // Consume newline
+            String userJson = IUserDao.findUserJsonByStudentId(studentId);
+            System.out.println(userJson);
+        } catch (DaoException e) {
+            System.out.println("Error: " + e.getMessage());
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid input. Please enter a valid student ID.");
+            scanner.nextLine(); // Clear the invalid input from the scanner
         }
     }
 }
